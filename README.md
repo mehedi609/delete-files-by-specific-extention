552b2fa8f8b19681fac2ff6b2bc4f1536ccd8994
mehedi609 <35611282+mehedi609@users.noreply.github.com>
2019-09-02 20:22:09 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Delete specific files from a directory and it's subdirectory
---
+++ delete_files.py
@@ -0,0 +1,13 @@
+import os
+import fnmatch
+
+dir_path = 'E:\\Web Development Tutorials\\PHP Framework\\Real Time Single Page Forum App with Pusher Laravel & vuejs'
+
+# Get a list of all files in directory
+for rootDir, subdirs, filenames in os.walk(dir_path):
+    # Find the files that matches the given patterm
+    for filename in fnmatch.filter(filenames, '*.vtt'):
+        try:
+            os.remove(os.path.join(rootDir, filename))
+        except OSError:
+            print("Error while deleting file")
